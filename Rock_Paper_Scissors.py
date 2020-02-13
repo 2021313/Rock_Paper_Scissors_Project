ac80a1f1f51b7e977a196355d6863dd4d6367d26
Torana186 <dgpainton@gmail.com>
2020-02-13 14:39:45 +1300

    Update Rock_Paper_Scissors.py

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -19,20 +19,8 @@ r_num = int(input('Please specify how many rounds you want to play: '))
 for r_count in range(r_num):
         #Get players choices
         p1_choice = getpass.getpass(p1 + ' please choose between Rock(1), Paper(2) and Scissors(3): ')
-        if p1_choice == '1':
-                print('Do not throw things!')
-        elif p1_choice == '2':
-                print('Lets make origami!')
-        elif p1_choice == '3':
-                print('Time for some arts and crafts!')
         print('Player 1 has chosen')
         p2_choice = getpass.getpass(p2 + ' please choose between Rock, Paper and Scissors: ')
-        if p2_choice == '1':
-                print('Do not throw things!')
-        elif p2_choice == '2':
-                print('Lets make origami!')
-        elif p2_choice == '3':
-                print('Time for some arts and crafts!')
         print('Player 2 has chosen')
         p1_r_win = 0
         p2_r_win = 0
